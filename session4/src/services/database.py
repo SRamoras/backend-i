da0bd6cb7351e0c056f773e4bcc45fc079c74fac
S1lva <diogomcsilva5@gmail.com>
2026-03-03 16:06:57 +0000

    feat: add more progress on session4

--- session4/src/services/database.py
+++ session4/src/services/database.py
@@ -23,33 +23,38 @@
 
 
 
-
-
-
-
-
-
 from data.models import Meeting
 from uuid import uuid4
-
-BASE_PATH = "meetings"
+from pathlib import Path
+import json
+BASE_PATH = Path("meetings") 
+INDEX_PATH = Path("meetings/index.json")
 
 def create(meeting: Meeting):
     id = uuid4()
     fileName = f"{BASE_PATH}/{id}.md"
-    fileJson = f"storage.json"
 
     with open(fileName, "w") as file:
         file.writelines(str(meeting))
 
-    with open(fileJson,'r') as file:
-        content = file.read()
 
-    with open(fileJson, "w") as file:
-        file.writelines(f"""{content[:1]}
-{{"path": "{fileName}",
-"title": "{meeting.title}",
-"owner": "{meeting.owner}",
-"date": "{meeting.date}"
-}}{content[1:]}
-""")    
+
+    if not INDEX_PATH.exists():
+        INDEX_PATH.touch()
+
+    with open(INDEX_PATH,'r') as file:
+        if INDEX_PATH.stat().st_size > 0:
+            index_content = json.load(file)
+        else:
+            index_content = []
+            
+    new_index_entry = {
+        "path": fileName,
+        "title": meeting.title,
+        "owner": meeting.owner,
+        "date": meeting.date
+    }
+    index_content.append(new_index_entry)
+
+    with open(INDEX_PATH, "w") as file:
+        json.dump(index_content, file, indent=4)
