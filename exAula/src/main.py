7735def722dfc2e821bb82605220efee572fb53c
Diogo Silva <120826220+SRamoras@users.noreply.github.com>
2026-03-04 20:04:40 +0000

    feat: add create command

--- exAula/src/main.py
+++ exAula/src/main.py
@@ -1,12 +1,19 @@
 from typer import Typer
-from services import car
+from services import car_service
 from data.models import Car
+
 cli = Typer()
 
 
 @cli.command()
 def create(registration: str, model: str, price: float, date: str):
-    car.create(registration, model, price, date)
+    newCar = Car(
+        registration=registration,
+        model=model,
+        price=price,
+        date=date
+    )
+    car_service.create(car = newCar)
 
 if __name__ == "__main__":
     cli()
---
+++ exAula/src/services/car_service.py
@@ -0,0 +1,38 @@
+from data.models import Car
+from uuid import uuid4
+from pathlib import Path
+from dataclasses import asdict
+import json
+
+
+BASE_PATH = Path("stand")
+INDEX_PATH = Path(BASE_PATH/"index_stand.json")
+
+
+def create(car: Car):
+    
+    carFile = f"{BASE_PATH}/{uuid4()}.md"
+
+    with open(carFile, "w") as file:
+        file.writelines(str(car))
+
+    if not INDEX_PATH.exists():
+        INDEX_PATH.touch()
+        INDEX_PATH.write_text("[]")
+
+    with open(INDEX_PATH, "r") as file:
+        indexFileContent:list = json.load(file)
+
+    newIdex = asdict(
+        Car(
+            registration=car.registration,
+            model=car.model,
+            price=car.price,
+            date=car.date,
+        )
+    )
+    indexFileContent.append(newIdex)
+
+
+    with open(INDEX_PATH, "w") as file:
+        json.dump(indexFileContent, file)
